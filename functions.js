507a634f80b326c54daf37f97763ad5197db052e
code-in-time@hotmail.com <andrew.crawford.za@gmail.com>
2020-02-06 21:03:01 +0200

    Add more tests

--- functions.js
+++ functions.js
@@ -1,5 +1,9 @@
 const functions = {
-  add: (n1, n2) => n1 + n2 
+  add: (n1, n2) => n1 + n2,
+
+  isNull: () => null,
+
+  checkValue: x => x
 }
 
 module.exports = functions;
--- functions.test.js
+++ functions.test.js
@@ -4,3 +4,19 @@ const functions = require('./functions');
 test('Add 2 plus 2 to = 4', () => {
     expect(functions.add(2, 2)).toBe(4);
 });
+
+test('Add 2 plus 2 to no = 5', () => {
+  expect(functions.add(2, 2)).not.toBe(5);
+});
+
+test('to be null', () => {
+  expect(functions.isNull()).toBeNull();
+});
+
+test('it should be  false', () => {
+  expect(functions.checkValue(false)).toBeFalsy()
+});
+
+test('it should be  true', () => {
+  expect(functions.checkValue(true)).toBeTruthy()
+})
